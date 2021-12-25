a79e6f5e629a5bca15a86a64d0fd1a7299432734
jweir136 <jweir136@yahoo.com>
2021-12-25 16:51:32 -0500

    add node parsing function

--- src/parser.py
+++ src/parser.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 class OSMGraphParser:
     def __init__(self, filename):
         self.roads = []
+        self.nodes = {}
         self.VALID_ROADS = ["motorway", "trunk", "primary", "secondary", "tertiary", "unclassified", "residential"]
         buffer = ""
 
@@ -17,6 +18,42 @@ class OSMGraphParser:
                     buffer = ""
                     if self._is_valid_road(contains_way.group(0)):
                         self.roads.append(self._parse_way(contains_way.group(0)))
+
+                contains_node = self._contains_node(buffer)
+                if contains_node:
+                    buffer = ""
+                    node = self._parse_node(contains_node.group(0))
+                    self.nodes[node['id']] = node
+
+
+    def _contains_node(self, buffer):
+        node_reg1 = re.compile(r'<node .*/>')
+        node_reg2 = re.compile(r'<node [\s\S]*</node>')
+
+        node_reg1_search = node_reg1.search(buffer)
+        node_reg2_search = node_reg2.search(buffer)
+
+        if node_reg1_search:
+            return node_reg1_search
+        elif node_reg2_search:
+            return node_reg2_search
+        else:
+            return None
+
+    def _parse_node(self, node):
+        doc = xml.etree.ElementTree.fromstring(node)
+
+        node = {}
+        node['id'] = doc.attrib['id']
+        node['visible'] = doc.attrib['visible']
+        node['lon'] = doc.attrib['lon']
+        node['lat'] = doc.attrib['lat']
+        node['tags'] = {}
+
+        for tag in doc.findall("tag"):
+            node['tags'][tag.attrib['k']] = tag.attrib['v']
+
+        return node
                 
     def _contains_way(self, buffer):
         way_reg = re.compile(r'<way [\s\S]*</way>')
@@ -34,9 +71,6 @@ class OSMGraphParser:
         way_dict['tags'] = {}
         for tag in doc.findall("tag"):
             way_dict['tags'][tag.attrib['k']] = tag.attrib['v']
-        way_dict['nodes'] = [x.attrib['ref'] for x in doc.findall("nd")]
+        way_dict['nodes'] = [self.nodes[x.attrib['ref']] for x in doc.findall("nd")]
         return way_dict
-
-if __name__ == "__main__":
-    print(OSMGraphParser("test-data/map-2.osm").roads)
     
